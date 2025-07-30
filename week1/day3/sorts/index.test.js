be0cbcfbe8aba3edf47df2b4775667336f3703a9
c0derec0de <uli4n4.4@yandex.ru>
2025-07-30 16:41:44 +0300

    applied DRY

--- week1/day3/sorts/index.test.js
+++ week1/day3/sorts/index.test.js
@@ -3,77 +3,67 @@
 import { bubbleSort, qSort } from "./index.js";
 import arrGenerator from "./arrGenerator.js";
 
+const testMocks = [
+  {
+    title: "Сортируем массив [1, 4, 2, 8] в [1, 2, 4, 8]",
+    mock: [1, 4, 2, 8],
+    result: [1, 2, 4, 8],
+  },
+  { title: "Единичный массив [1] в [1]", mock: [1], result: [1] },
+  { title: "Пустой массив [] в []", mock: [], result: [] },
+  {
+    title: "Массив с дубликатами [1, 6, 6, 8]",
+    mock: [1, 6, 6, 8],
+    result: [1, 6, 6, 8],
+  },
+  {
+    title: "Массив с отрицательными значениями [-1, -6, -6, 8]",
+    mock: [-1, -6, -6, 8],
+    result: [-6, -6, -1, 8],
+  },
+  {
+    title: "Отсортированный массив [1, 2, 4, 8]",
+    mock: [1, 2, 4, 8],
+    result: [1, 2, 4, 8],
+  },
+  {
+    title: "Все элементы одинаковые [5, 5, 5]",
+    mock: [5, 5, 5],
+    result: [5, 5, 5],
+  },
+];
 // bubbleSort
-test("bubbleSort. Сортируем массив [1, 4, 2, 8] в [1, 2, 4, 8]", () => {
-  expect(bubbleSort([1, 4, 2, 8])).toEqual([1, 2, 4, 8]);
-});
 
-test("bubbleSort. Единичный массив [1] в [1]", () => {
-  expect(bubbleSort([1])).toEqual([1]);
-});
+describe("bubbleSort", () => {
+  testMocks.forEach(({ title, mock, result }) => {
+    test(title, () => {
+      expect(bubbleSort(mock)).toEqual(result);
+    });
+  });
 
-test("bubbleSort. Пустой массив [] в []", () => {
-  expect(bubbleSort([])).toEqual([]);
-});
-
-test("bubbleSort. Массив с дубликатами [1, 4, 4, 8]", () => {
-  expect(bubbleSort([1, 4, 4, 8])).toEqual([1, 4, 4, 8]);
-});
-
-test("bubbleSort. Массив с отрицательными значениями [-1, -4, -4, 8]", () => {
-  expect(bubbleSort([-4, -4, -1, 8])).toEqual([-4, -4, -1, 8]);
-});
-
-test("bubbleSort. Отсортированный массив [1, 2, 4, 8]", () => {
-  expect(bubbleSort([1, 2, 4, 8])).toEqual([1, 2, 4, 8]);
-});
-
-test("bubbleSort. Все элементы одинаковые [5, 5, 5]", () => {
-  expect(bubbleSort([5, 5, 5])).toEqual([5, 5, 5]);
-});
-
-test("bubbleSort. Сравнение с встроенной сортировкой для [1, 4, 3, 7, 2]", () => {
-  const sorted = [1, 4, 3, 7, 2].sort((a, b) => a - b);
-  expect(bubbleSort([1, 4, 3, 7, 2])).toEqual(sorted);
+  test("Сравнение со встроенной сортировкой", () => {
+    const arr = [3, 1, 4, 2];
+    expect(bubbleSort(arr)).toEqual([...arr].sort((a, b) => a - b));
+  });
 });
 
 // qSort
-console.log("============qSort===========");
-test("qSort. Сортируем массив [1, 4, 2, 8] в [1, 2, 4, 8]", () => {
-  expect(qSort([1, 4, 2, 8])).toEqual([1, 2, 4, 8]);
-});
 
-test("qSort. Единичный массив [1] в [1]", () => {
-  expect(qSort([1])).toEqual([1]);
-});
+describe("qSort", () => {
+  testMocks.forEach(({ title, mock, result }) => {
+    test(title, () => {
+      expect(qSort(mock)).toEqual(result);
+    });
+  });
 
-test("qSort. Пустой массив [] в []", () => {
-  expect(qSort([])).toEqual([]);
-});
+  test("Сравнение со встроенной сортировкой", () => {
+    const arr = [3, 1, 4, 2];
+    expect(qSort(arr)).toEqual([...arr].sort((a, b) => a - b));
+  });
 
-test("qSort. Массив с дубликатами [1, 4, 4, 8]", () => {
-  expect(qSort([1, 4, 4, 8])).toEqual([1, 4, 4, 8]);
-});
-
-test("qSort. Массив с отрицательными значениями [-1, -4, -4, 8]", () => {
-  expect(qSort([-4, -4, -1, 8])).toEqual([-4, -4, -1, 8]);
-});
-
-test("qSort. Отсортированный массив [1, 2, 4, 8]", () => {
-  expect(qSort([1, 2, 4, 8])).toEqual([1, 2, 4, 8]);
-});
-
-test("qSort. Все элементы одинаковые [5, 5, 5]", () => {
-  expect(qSort([5, 5, 5])).toEqual([5, 5, 5]);
-});
-
-test("qSort. Сравнение с встроенной сортировкой для [1, 4, 3, 7, 2]", () => {
-  const sorted = [1, 4, 3, 7, 2].sort((a, b) => a - b);
-  expect(qSort([1, 4, 3, 7, 2])).toEqual(sorted);
-});
-
-test("qSort. Большой массив на 1000 элементов", () => {
-  const arr = arrGenerator(1000);
-  const sorted = [...arr].sort((a, b) => a - b);
-  expect(qSort(arr)).toEqual(sorted);
+  test("Большой массив на 1000 элементов", () => {
+    const arr = arrGenerator(1000);
+    const sorted = [...arr].sort((a, b) => a - b);
+    expect(qSort(arr)).toEqual(sorted);
+  });
 });
