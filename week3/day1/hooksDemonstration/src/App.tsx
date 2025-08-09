09b6f08c50a4ddf69ba809c8f81032703f42983a
c0derec0de <uli4n4.4@yandex.ru>
2025-08-09 18:28:51 +0300

    fix: divided and added mountion

--- week3/day1/hooksDemonstration/src/App.tsx
+++ week3/day1/hooksDemonstration/src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import "./App.css";
-import { Demo } from "./components/Demo";
+import { Mounting } from "./components/Mounting/Mounting";
+import { VisualDifference } from "./components/VisualDifference/VisualDifference";
 
 function App() {
+  const [visible, setVisible] = useState(true);
+
   return (
-    <div className="App">
-      <div className="demo-section">
-        <Demo />
+    <div style={{ display: "flex", flexDirection: "row", gap: "100px" }}>
+      <div style={{ border: "2px solid purple", padding: "20px" }}>
+        <button
+          style={{ marginBottom: "10px" }}
+          onClick={() => setVisible(visible ? false : true)}
+        >
+          {visible ? "Размонтировать компонент" : "Монтировать компонент"}
+        </button>
+        <div className="mounting-section">{visible && <Mounting />}</div>
+      </div>
+      <div style={{ border: "2px solid purple", padding: "20px" }}>
+        <VisualDifference />
       </div>
     </div>
   );
---
+++ week3/day1/hooksDemonstration/src/components/Mounting/Mounting.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+
+export const Mounting = () => {
+  const [mountCounter, setMountCounter] = useState(0);
+
+  const [updateId, setUpdateId] = useState(1);
+  const [updateCounter, setUpdateCounter] = useState(0);
+
+  // клинап при размонтировании
+  useEffect(() => {
+    console.log("Компонент монтирован. Запуск интервала.");
+
+    const intervalId = setInterval(() => {
+      setMountCounter((c) => c + 1);
+    }, 1000);
+
+    return () => {
+      console.log(
+        "%cКомпонент размонтирован. Cleanup после размонтирования.",
+        "color: red; font-weight: bold;"
+      );
+      clearInterval(intervalId);
+    };
+  }, []); // сработает 1 раз при монтировании и 1 раз при размонтировании
+
+  // клинап при обновлении
+  useEffect(() => {
+    console.log(`Запуск счетчика для ID: ${updateId}`);
+
+    const intervalId = setInterval(() => {
+      setUpdateCounter((c) => c + 1);
+    }, 1000);
+
+    return () => {
+      console.log(
+        `%cКомпонент размонтирован. Cleanup после обновления [updateId]`,
+        "color: red; font-weight: bold;"
+      );
+      clearInterval(intervalId);
+    };
+  }, [updateId]); //  эффект перезапустится при изменении updateId
+
+  const handleRestart = () => {
+    setUpdateCounter(0);
+    setUpdateId((id) => id + 1);
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <h3>Cleanup при размонтировании</h3>
+        <p>Этот счетчик запущен в useEffect с пустым [].</p>
+        <p>
+          Счетчик: <strong>{mountCounter}</strong>
+        </p>
+      </div>
+      <hr />
+
+      <div className="container">
+        <h3>Cleanup при Обновлении</h3>
+        <p> Этот счетчик запущен в useEffect и зависит от [updateId]</p>
+        <p>
+          Счетчик: <strong>{updateCounter}</strong>
+        </p>
+        <button onClick={handleRestart}>Начать заново</button>
+      </div>
+    </div>
+  );
+};
