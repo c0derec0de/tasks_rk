a160fa46333db78c49fff02ae40d7ccca4e2429a
c0derec0de <uli4n4.4@yandex.ru>
2025-08-19 20:38:35 +0300

    feat: add delete and add post

--- week4/day3/listPosts/src/components/List.tsx
+++ week4/day3/listPosts/src/components/List.tsx
@@ -1,22 +1,45 @@
 import { model } from "../app/store/rootEffector";
 import { createGate, useGate, useList, useUnit } from "effector-react";
+import type { Post } from "../types/types";
 
 const Gate = createGate();
 Gate.status.watch((opened) => console.info("is Gate opened?", opened));
 
 export const List: React.FC = () => {
   useGate(Gate);
-  const handleGetPosts = useUnit(model.getPost);
+
+  const [posts, handleGetPosts, handleAddNewPost, handleDelete] = useUnit([
+    model.$posts,
+    model.getPost,
+    model.addPost,
+    model.deletePost,
+  ]);
+
+  const post: Post = {
+    userId: 1,
+    id: Date.now() + posts.length,
+    title: "Title",
+    body: "Body",
+  };
 
   const list = useList(model.$posts, (post, index) => (
     <li key={post.id}>
       [{index}] {post.title}
+      <button
+        onClick={() => handleDelete(post.id)}
+        style={{ marginLeft: "20px" }}
+      >
+        Удалить пост
+      </button>
     </li>
   ));
 
   return (
     <div>
-      <button onClick={handleGetPosts}>Загрузить посты</button>
+      <button onClick={handleGetPosts} style={{ marginTop: "20px" }}>
+        Загрузить посты
+      </button>
+      <button onClick={() => handleAddNewPost(post)}>Добавить пост</button>
       <ul>{list}</ul>
     </div>
   );
