d7b8c104fe880dd979fb5a6d86c0bcd720193d6e
c0derec0de <uli4n4.4@yandex.ru>
2025-08-29 12:56:01 +0300

    feat: add XHRfetch

--- week5/day5/dataLoader/src/App.tsx
+++ week5/day5/dataLoader/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { TestList } from "./components/testList";
+import { TestXHRTest } from "./components/TestXHRTest";
 
 function App() {
   const [mounting, setMounting] = useState(true);
@@ -10,6 +11,7 @@ function App() {
       <div>
         <button onClick={() => setMounting(!mounting)}>Видимость</button>
         {mounting && <TestList />}
+        {mounting && <TestXHRTest />}
       </div>
     </>
   );
