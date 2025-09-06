8db297cfb9336c1054f5ac0dcb88d87bc757dc3b
c0derec0de <uli4n4.4@yandex.ru>
2025-09-06 21:17:41 +0300

    fix: remove week7/day1

--- week7/day1/firstapp/.next/server/pages/home/user.js
+++
@@ -1,8 +0,0 @@
-var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/pages/home/user.js")
-R.c("server/chunks/ssr/node_modules_0b53a5be._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__9f350845._.js")
-R.c("server/chunks/ssr/pages__app_tsx_b43847ce._.js")
-R.c("server/chunks/ssr/node_modules_43bce806._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__1b562f8b._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/home/user.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/pages/home/user.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
